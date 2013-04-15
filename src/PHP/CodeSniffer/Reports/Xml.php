96869f44de5e1b03912bb91c28f85140495b454f
Sergei Morozov <morozov@tut.by>
2013-04-15 22:36:45 +0300

    Reworked for compatibility with PHP_CodeSniffer 1.5.

--- src/PHP/CodeSniffer/Reports/Xml.php
+++ src/PHP/CodeSniffer/Reports/Xml.php
@@ -82,22 +82,22 @@ class PHP_CodeSniffer_Reports_Xml implements PHP_CodeSniffer_Report
     }
 
     /**
-     * Prints errors and warnings found only in lines modified in commit.
+     * Generate a partial report for a single processed file.
      *
-     * Errors and warnings are displayed together, grouped by file.
+     * Function should return TRUE if it printed or stored data about the file
+     * and FALSE if it ignored the file. Returning TRUE indicates that the file and
+     * its data should be counted in the grand totals.
      *
-     * @param array   $report      Prepared report.
+     * @param array   $report      Prepared report data.
      * @param boolean $showSources Show sources?
-     * @param int     $width       Maximum allowed lne width.
-     * @param boolean $toScreen    Is the report being printed to screen?
+     * @param int     $width       Maximum allowed line width.
      *
-     * @return string
+     * @return boolean
      */
-    public function generate(
+    public function generateFileReport(
         $report,
         $showSources=false,
-        $width=80,
-        $toScreen=true
+        $width=80
     ) {
         $diff = $this->getStagedDiff();
         $changes = $this->getChanges($diff);
@@ -105,8 +105,42 @@ class PHP_CodeSniffer_Reports_Xml implements PHP_CodeSniffer_Report
         $report = $this->filterReport($report, $changes);
 
         $full = new PHP_CodeSniffer_Reports_Full();
-        return $full->generate($report, $showSources, $width, $toScreen);
+        return $full->generateFileReport($report, $showSources, $width);
     }
+
+    /**
+     * Prints all errors and warnings for each file processed.
+     *
+     * @param string  $cachedData    Any partial report data that was returned from
+     *                               generateFileReport during the run.
+     * @param int     $totalFiles    Total number of files processed during the run.
+     * @param int     $totalErrors   Total number of errors found during the run.
+     * @param int     $totalWarnings Total number of warnings found during the run.
+     * @param boolean $showSources   Show sources?
+     * @param int     $width         Maximum allowed line width.
+     * @param boolean $toScreen      Is the report being printed to screen?
+     *
+     * @return void
+     */
+    public function generate(
+        $cachedData,
+        $totalFiles,
+        $totalErrors,
+        $totalWarnings,
+        $showSources=false,
+        $width=80,
+        $toScreen=true
+    ) {
+        echo $cachedData;
+
+        if ($toScreen === true
+            && PHP_CODESNIFFER_INTERACTIVE === false
+            && class_exists('PHP_Timer', false) === true
+        ) {
+            echo PHP_Timer::resourceUsage().PHP_EOL.PHP_EOL;
+        }
+
+    }//end generate()
 
     /**
      * Returns staged content diff
@@ -173,61 +207,53 @@ class PHP_CodeSniffer_Reports_Xml implements PHP_CodeSniffer_Report
      */
     protected function filterReport(array $report, array $changes)
     {
-        $files = array();
-        foreach ($changes as $path => $lines) {
-            $tmpPath = $this->tmpDir . DIRECTORY_SEPARATOR . $path;
-
-            if (isset($report['files'][$tmpPath])) {
-                $files[$path]['messages'] = array_intersect_key(
-                    $report['files'][$tmpPath]['messages'],
-                    array_flip($lines)
+        $filename = $report['filename'];
+        if (strpos($filename, $this->tmpDir . DIRECTORY_SEPARATOR) === 0) {
+            $relative = substr($filename, strlen($this->tmpDir) + 1);
+            if (isset($changes[$relative])) {
+                $report['messages'] = array_intersect_key(
+                    $report['messages'],
+                    array_flip($changes[$relative])
                 );
             }
         }
 
-        return $this->getReport($files);
+        return $this->repairReport($report);
     }
 
     /**
      * Generates report from file data
      *
-     * @param array $files File data
+     * @param array $report File data
      *
      * @return array
      */
-    protected function getReport(array $files)
+    protected function repairReport(array $report)
     {
-        $totals = array(
-            'warnings' => 0,
+        $repaired = array_merge($report, array(
             'errors'   => 0,
-        );
+            'warnings' => 0,
+        ));
 
-        foreach ($files as $path => $file) {
-            $files[$path] = array_merge($file, $totals);
-            foreach ($file['messages'] as $columns) {
-                foreach ($columns as $messages) {
-                    foreach ($messages as $message) {
-                        switch($message['type']) {
+        foreach ($report['messages'] as $columns) {
+            foreach ($columns as $messages) {
+                foreach ($messages as $message) {
+                    switch($message['type']) {
                         case 'ERROR';
-                                $key = 'errors';
+                            $key = 'errors';
                             break;
                         case 'WARNING';
-                                $key = 'warnings';
+                            $key = 'warnings';
                             break;
                         default;
                             $key = null;
                             continue;
-                        }
-                        $files[$path][$key]++;
-                        $totals[$key]++;
                     }
+                    $repaired[$key]++;
                 }
             }
         }
 
-        return array(
-            'totals' => $totals,
-            'files'  => $files,
-        );
+        return $repaired;
     }
 }
